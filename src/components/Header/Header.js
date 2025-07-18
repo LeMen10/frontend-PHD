9ce4a4aa9551bd0264638e6d507e145e6ac7cff9
menvo <volemenn@gmail.com>
2025-07-18 11:19:58 +0700

    FAQs and values

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
 import { Link } from 'react-router-dom';
-import images from '~/assets/images/images';
+// import images from '~/assets/images/images';
 
 const cx = classNames.bind(styles);
 
